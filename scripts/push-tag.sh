1f0977dc7c6633aa1fa06cd25184e276309ab2c8
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-10 14:58:43 +0300

    feature/devcontainer-cli/1

--- scripts/push-tag.sh
+++ scripts/push-tag.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 branch=$(git branch --show-current)
+
+
+if [[ $branch != feature/* ]];
+then
+  echo "This script can only be used on feature branches"
+  echo "Current branch: $branch"
+  exit 1
+fi
+
 last_tag=$(git tag --list "$branch/*" | sort | tail -n1)
 echo "last_tag: $last_tag"
 last_number=${last_tag##*/}
@@ -9,5 +18,5 @@ next_number=$((last_number + 1))
 message="$branch/$next_number"
 git add .
 git commit -m $message
-git tag -a $message -m "$message"
+git tag -a $message -m "This tag's artifacts are not meant for consumption"
 git push --follow-tags
